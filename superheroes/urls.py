9b39dd126b6b5383f1ca95381052987f3d49f855
Luca Leyva <imlucaleyva@gmail.com>
2021-10-22 15:38:34 -0400

    Added update_hero page.

--- superheroes/urls.py
+++ superheroes/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from django.urls.resolvers import URLPattern
 from . import views
 
+
 app_name = 'superheroes'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:hero_id>/', views.detail, name='detail'),
-    path('new/', views.create, name='create')
-]
+    path('new/', views.create, name='create'),
+    path('update_hero/<int:hero_id>/', views.update_hero, name='update_hero')]
--- superheroes/views.py
+++ superheroes/views.py
@@ -10,7 +10,7 @@ def index(request):
         'all_heroes': all_heroes
     }
     return render(request, 'superheroes/index.html', context)
-
+# 4
 def detail(request, hero_id):
     single_hero = Superhero.objects.get(pk=hero_id) 
     context = {
@@ -31,5 +31,30 @@ def create(request):
         new_hero = Superhero(name=name, alter_ego=alter_ego, primary_ability=primary_ability, secondary_ability=secondary_ability, catch_phrase=catch_phrase)
         new_hero.save()
         return HttpResponseRedirect(reverse('superheroes:index'))
-    else:
+    elif request.method == 'GET':
         return render(request, 'superheroes/create.html')
+
+def update_hero(request, hero_id):
+    print(request.method)
+    # If request.method is POST 
+        # Get the superhero from the database using the "hero_id" parameter value
+    if request.method == 'POST':
+        update_hero = Superhero.objects.get(pk=hero_id) 
+        update_hero.name = request.POST.get('name')
+        update_hero.alter_ego = request.POST.get('alter_ego')
+        update_hero.primary_ability = request.POST.get('primary_ability')
+        update_hero.secondary_ability = request.POST.get('secondary_ability')
+        update_hero.catch_phrase = request.POST.get('catch_phrase')
+        update_hero.save()
+        return HttpResponseRedirect(reverse('superheroes:index'))
+        
+    # If request.method is GET
+        # Get the superhero from the database using the "hero_id" parameter value
+        # Create a context object with that superhero from the db inside of it
+        # Bring up the update.html page and send in the superhero object
+    elif request.method == 'GET':
+        update_hero = Superhero.objects.get(pk=hero_id) 
+        context = {
+            'update_hero': update_hero
+        }
+        return render(request, 'superheroes/update_hero.html', context)
